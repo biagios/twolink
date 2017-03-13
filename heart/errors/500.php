5693ef3d1c851bc2743c178aa8e7652afbdefa4f
Biagio Santoro <Sleme@users.noreply.github.com>
2017-03-13 22:23:04 +0000

    Add files via upload

---
+++ heart/errors/500.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Linker | 500 Error</title>
+	<?php include "$_SERVER[DOCUMENT_ROOT]/../public/heart/meta.php";?>
+	<div class="site-wrapper">
+		<?php include "$_SERVER[DOCUMENT_ROOT]/heart/fade.min.php";?>
+		<div class="site-wrapper-inner">
+			<div class="cover-container">
+				<div class="hidden">
+				<div class="masthead clearfix">
+					<div class="inner">
+
+					</div>
+				</div>
+
+				<div class="inner cover">
+					<h1 class="cover-heading">
+						<div id="clockbox">
+							500
+						</div>
+					</h1>
+					<p class="leadd">
+						<h2>
+							<img src="/heart/img/errors/robit.png" alt="Robit" draggable="false">
+              <p> Ouch, our servers are acting up.
+<p>We are trying to fix this issue, try again later.
+			      </h2>
+					</p>
+					<p class="lead"> <a onclick="history.back(-1)" class="btn btn-lg btn btn-outline-success">Safety!</a> </p>
+				</div>
+				<?php include "$_SERVER[DOCUMENT_ROOT]/heart/foot.php";?>
+				</div>
+			</div>
+		</div>
+	</div>
+	</body>
+
+</html>
